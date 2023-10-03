d6eac27e9e6dec8b4bf2f3f3850fe104164e3a09
naoise collins <naoisecollins@gmail.com>
2023-10-03 21:18:31 +0100

    add DataTypes

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@ int main()
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    int screenWidth = 800;
-    int screenHeight = 500;
 
-    InitWindow(screenWidth, screenHeight, "Empty Window with Raylib");
+    InitWindow(800, 600, "Data Types");
+
+    int myInteger = 5;
+    float myFloat = 3.14f;
+    bool isProgrammingFun = true;
+    char myCharacter = 'A';
+    double myDouble = 2.71828;
 
     SetTargetFPS(60);               // Set the desired frames-per-second target
 
@@ -27,6 +31,11 @@ int main()
 
         ClearBackground(RAYWHITE);  // Clear the screen with a white color
 
+        DrawText("Hello, Raylib", 10,10,20,BLACK);
+        DrawText(TextFormat("Integer Value: %d", myInteger), 10,40,20,BLACK);
+        DrawText(TextFormat("Float Value: %f", myFloat), 10,70,20,BLACK);
+        DrawText(TextFormat("Double Value: %f", myDouble), 10, 100,20,BLACK);
+        DrawText(TextFormat("Character Value: %c", myCharacter), 10,130,20,BLACK);
         // TODO: Draw everything you want here
 
         EndDrawing();
