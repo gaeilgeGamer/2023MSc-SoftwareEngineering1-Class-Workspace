93c4a72e766e28f3810915f8e5a6a6bd05d0368b
naoise collins <naoisecollins@gmail.com>
2023-10-17 21:03:35 +0100

    Added Function Example

--- main.cpp
+++ main.cpp
@@ -1,53 +1,37 @@
 #include "raylib.h"
 
+const int screenWidth = 800;
+const int screenHeight = 600;
+Vector2 circlePosition = {screenWidth/2, screenHeight/2};
+const float moveSpeed = 5.0f;
+const int circleRadius = 50;
+
+Vector2 MoveCircle(Vector2 position)
+{
+    if(IsKeyDown(KEY_RIGHT)) position.x += moveSpeed; 
+    if(IsKeyDown(KEY_LEFT)) position.x -= moveSpeed;
+    if(IsKeyDown(KEY_UP)) position.y -= moveSpeed;
+    if(IsKeyDown(KEY_DOWN)) position.y += moveSpeed;
+    return position; 
+}
+
 int main()
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    InitWindow(screenWidth, screenHeight, "For Loops");
-    
+    InitWindow(screenWidth, screenHeight, "Function Example");
     SetTargetFPS(60);
     
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        circlePosition = MoveCircle(circlePosition);
+
             BeginDrawing();
             ClearBackground(RAYWHITE);
-            
-            for(int i = 0; i <5; i++){
-                Color squareColor;
-
-                switch(i){
-                    case 0:
-                        squareColor = (Color){0,0,50,255};
-                        break;
-                    case 1:
-                        squareColor = (Color){0,0,90,255};
-                        break;
-                    case 2:
-                        squareColor = (Color){0,0,130,255};
-                        break;
-                    case 3:
-                        squareColor = (Color){0,0,170,255};
-                        break;
-                    case 4:
-                        squareColor = (Color){0,0,255,255};
-                        break;
-                    default:
-                        squareColor = (Color){0,0,255,255};
-                        break;
-                }
-                DrawRectangle(100+100 * i, 250,50,50, squareColor);
-            }
-            DrawText("For Loop Example with squares!", 10,10,20,DARKGRAY);
-            EndDrawing();
+            DrawCircleV(circlePosition, circleRadius, RED);
+            EndDrawing();        
+                
     }
-   
-    CloseWindow();        // Close the window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
+    CloseWindow();       
+    return 0;       
+            
 }
