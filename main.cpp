c53e2a0a91978c0b47e13062ebc2aced561df742
naoise collins <naoisecollins@gmail.com>
2023-10-17 20:37:10 +0100

    Add switch statement

--- main.cpp
+++ main.cpp
@@ -7,23 +7,46 @@ int main()
     const int screenWidth = 800;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "Audio Example");
+    InitWindow(screenWidth, screenHeight, "Switch Case Example");
     
-    InitAudioDevice();
+    Texture2D myTexture = LoadTexture("Resources/Textures/mario.png");
 
-    Sound sound = LoadSound("Resources/Sounds/sound.wav");
+    Vector2 scale = {0.5f, 0.5f};
 
-    int volume = 50;
-    SetSoundVolume(sound, volume/100.0f);
+    Color tint = WHITE;
     
     SetTargetFPS(60);               // Set the desired frames-per-second target
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-    
+        Vector2 position = GetMousePosition();
+        position.x -= myTexture.width * scale.x/2;
+        position.y -= myTexture.height * scale.y/2;
+
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+            tint= YELLOW;
+        }
+        if(IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)){
+            tint= WHITE;
+        }
+        if(IsKeyPressed(KEY_ONE) || IsKeyPressed(KEY_TWO) || IsKeyPressed(KEY_THREE)) 
+        {
+            switch(GetKeyPressed()){
+                case KEY_ONE:
+                tint = RED;
+                break;
+                case KEY_TWO:
+                tint = BLUE;
+                break;
+                case KEY_THREE:
+                tint = GREEN;
+                break;
+            default:
+                break;
+            }
+
+        }
 
         // TODO: Update your variables here
         
@@ -34,19 +57,9 @@ int main()
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-        if(IsKeyPressed(KEY_SPACE)){
-            PlaySound(sound);
-        }
-        if(IsKeyPressed(KEY_UP)&& volume <100)
-        {
-            volume += 10;
-            SetSoundVolume(sound, volume /100.0f);
-        }
-                if(IsKeyPressed(KEY_DOWN)&& volume >0)
-        {
-            volume -= 10;
-            SetSoundVolume(sound, volume /100.0f);
-        }
+
+        DrawTextureEx(myTexture, position, 0, 0.5f, tint);
+       
 
           // Clear the screen with a white color
         // TODO: Draw everything you want here
@@ -57,10 +70,11 @@ int main()
         //----------------------------------------------------------------------------------
     }
 
+    UnloadTexture(myTexture);
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadSound(sound);
-    CloseAudioDevice();
+   
+
     CloseWindow();        // Close the window and OpenGL context
     //--------------------------------------------------------------------------------------
 
