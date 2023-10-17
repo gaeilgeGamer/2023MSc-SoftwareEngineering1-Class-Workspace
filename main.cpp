e2b7484e41abdfca888b30db2e28bfbc9740d54c
naoise collins <naoisecollins@gmail.com>
2023-10-17 20:51:44 +0100

    For Loop

--- main.cpp
+++ main.cpp
@@ -7,19 +7,42 @@ int main()
     const int screenWidth = 800;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "While Loops");
+    InitWindow(screenWidth, screenHeight, "For Loops");
     
-    int loopCounter = 0;
+    SetTargetFPS(60);
     
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-            loopCounter++;
-
             BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText(TextFormat("Loop Counter: %d", loopCounter), 10,10,20,LIGHTGRAY);
             
+            for(int i = 0; i <5; i++){
+                Color squareColor;
+
+                switch(i){
+                    case 0:
+                        squareColor = (Color){0,0,50,255};
+                        break;
+                    case 1:
+                        squareColor = (Color){0,0,90,255};
+                        break;
+                    case 2:
+                        squareColor = (Color){0,0,130,255};
+                        break;
+                    case 3:
+                        squareColor = (Color){0,0,170,255};
+                        break;
+                    case 4:
+                        squareColor = (Color){0,0,255,255};
+                        break;
+                    default:
+                        squareColor = (Color){0,0,255,255};
+                        break;
+                }
+                DrawRectangle(100+100 * i, 250,50,50, squareColor);
+            }
+            DrawText("For Loop Example with squares!", 10,10,20,DARKGRAY);
             EndDrawing();
     }
    
