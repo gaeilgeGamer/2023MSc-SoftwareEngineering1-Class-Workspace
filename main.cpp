9fef4e7cde904d2a6832a49adcba3959b9cd7a95
naoise collins <naoisecollins@gmail.com>
2023-10-17 20:42:35 +0100

    While Loop Example

--- main.cpp
+++ main.cpp
@@ -7,74 +7,22 @@ int main()
     const int screenWidth = 800;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "Switch Case Example");
+    InitWindow(screenWidth, screenHeight, "While Loops");
     
-    Texture2D myTexture = LoadTexture("Resources/Textures/mario.png");
-
-    Vector2 scale = {0.5f, 0.5f};
-
-    Color tint = WHITE;
+    int loopCounter = 0;
     
-    SetTargetFPS(60);               // Set the desired frames-per-second target
-
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        Vector2 position = GetMousePosition();
-        position.x -= myTexture.width * scale.x/2;
-        position.y -= myTexture.height * scale.y/2;
+            loopCounter++;
 
-        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-            tint= YELLOW;
-        }
-        if(IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)){
-            tint= WHITE;
-        }
-        if(IsKeyPressed(KEY_ONE) || IsKeyPressed(KEY_TWO) || IsKeyPressed(KEY_THREE)) 
-        {
-            switch(GetKeyPressed()){
-                case KEY_ONE:
-                tint = RED;
-                break;
-                case KEY_TWO:
-                tint = BLUE;
-                break;
-                case KEY_THREE:
-                tint = GREEN;
-                break;
-            default:
-                break;
-            }
-
-        }
-
-        // TODO: Update your variables here
-        
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawTextureEx(myTexture, position, 0, 0.5f, tint);
-       
-
-          // Clear the screen with a white color
-        // TODO: Draw everything you want here
-
-
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText(TextFormat("Loop Counter: %d", loopCounter), 10,10,20,LIGHTGRAY);
+            
+            EndDrawing();
     }
-
-    UnloadTexture(myTexture);
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
    
-
     CloseWindow();        // Close the window and OpenGL context
     //--------------------------------------------------------------------------------------
 
