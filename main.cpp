ef8fe2adfa03dae5ac82198f196b26e5bf0f5242
naoise collins <naoisecollins@gmail.com>
2023-10-24 21:41:51 +0100

    Basic Class Example

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
 #include "raylib.h"
 
-#define NUM_RECTANGLES 10
+class Ball{
+    public:
+    Vector2 position; 
+    float radius; 
+    Color color;
+
+    Ball(Vector2 pos, float r, Color c) : position(pos), radius(r), color(c){}
+
+    void Draw(){
+        DrawCircleV(position,radius, color);
+    }
+
+    void Move(Vector2 offset)
+    {
+        position.x += offset.x;
+        position.y += offset.y;
+    }
+
+    Vector2 GetPosition() const{
+        return position;
+    }
+
+    void SetPosition(Vector2 pos){
+        position = pos; 
+    }
+};
+
 
 int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 600;
     
-    InitWindow(screenWidth, screenHeight, "Iterating through an array of Rectangles");
+    InitWindow(screenWidth, screenHeight, "Class Example with a ball");
     
-    Rectangle rectangles[NUM_RECTANGLES];
-    Color rectangleColors[NUM_RECTANGLES];
-
-    for(int i = 0; i< NUM_RECTANGLES; i++)
-    {
-        rectangles[i].x = GetRandomValue(0, screenWidth-100);
-        rectangles[i].y = GetRandomValue(0, screenHeight-100);
-        rectangles[i].width = GetRandomValue(50,150);
-        rectangles[i].height = GetRandomValue(50,150);
-
-        rectangleColors[i] = (Color){GetRandomValue(50,255),GetRandomValue(50,255),GetRandomValue(50,255),255};
-
-    }
-
+    Ball myBall({screenWidth/2, screenHeight/2}, 50, BLUE);
+  
     SetTargetFPS(60);
     
     // Main game loop
@@ -32,11 +46,13 @@ int main()
             BeginDrawing();
             ClearBackground(RAYWHITE);
 
-            for(int i = 0; i<NUM_RECTANGLES; i++)
-            {
-            DrawRectangleRec(rectangles[i], rectangleColors[i]);
-            }
-            
+            myBall.Draw();
+
+            if(IsKeyDown(KEY_RIGHT)) myBall.Move({5,0});
+            if(IsKeyDown(KEY_LEFT)) myBall.Move({-5,0});
+            if(IsKeyDown(KEY_UP)) myBall.Move({0,-5});
+            if(IsKeyDown(KEY_DOWN)) myBall.Move({0,5});
+
             EndDrawing();        
                 
     }
