1f23a1130d33efc4ad6e58afee884ebcad9c3ccf
naoise collins <naoisecollins@gmail.com>
2023-11-07 21:06:01 +0000

    Sample with Encapsulation

--- main.cpp
+++ main.cpp
@@ -1,62 +1,51 @@
 #include "raylib.h"
 
-class Shape{
-    public:
-    int x,y;
-    Color color;
+class Paddle {
+    private:
+    int posX;
+    int posY;
+    int width;
+    int height; 
+    Color color; 
 
-    Shape(int x, int y, Color color): x(x), y(y), color(color){}
+    public: 
+    Paddle(int x, int y, int width, int height, Color color)
+    :posX(x), posY(y), width(width), height(height), color(color){}
+
+    void Draw() const{
+        DrawRectangle(posX,posY,width,height,color);
+    }
+    void Update(){
+        if(IsKeyDown(KEY_UP)){
+            posY -= 5;
+        }
+        if(IsKeyDown(KEY_DOWN)){
+            posY += 5;
+        }
+
+        if(posY<0) posY = 0;
+        if(posY>GetScreenHeight()-height) posY = GetScreenHeight() - height;
+    }
 };
-
-class RectangleShape: public Shape{
-public:
-int width,height; 
-
-RectangleShape(int x, int y, int width, int height, Color color)
-: Shape(x,y, color), width(width), height(height){}
-
-void Render(){
-    DrawRectangle(x,y, width, height, color);
-}
-
-};
-
-class CircleShape : public Shape{
-    public:
-    int radius;
-
-    CircleShape(int x, int y, int radius, Color color)
-    :Shape(x,y,color), radius(radius){}
-
-void Render(){
-    DrawCircle(x,y,radius,color);
-}
-};
-
 
 int main()
 {
     const int screenWidth = 800;
-    const int screenHeight = 600;
+    const int screenHeight = 450;
     
-    InitWindow(screenWidth, screenHeight, "Raylib with Inheritance");
-
-    RectangleShape rect(100,100,200,150, RED);
-
-    CircleShape circle(500,300,100,BLUE);
+    InitWindow(screenWidth, screenHeight, "Paddle Game");
   
+    Paddle playerPaddle(50, screenHeight/2-50,10,100,BLUE);
     SetTargetFPS(60);
     
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        playerPaddle.Update();
         
             BeginDrawing();
             ClearBackground(RAYWHITE);
-
-            rect.Render();
-            circle.Render();
-
+            playerPaddle.Draw();
             EndDrawing();        
                 
     }
